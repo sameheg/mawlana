b5c76b21b6c9bd8efacae63f17f46d9ed22634f7
sameheg <webyhost@gmail.com>
2025-09-06 17:01:03 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,33 +15,70 @@ use App\Http\Controllers\Procurement\PurchaseOrderController;
 use App\Http\Controllers\Procurement\SupplierController;
 use App\Http\Controllers\Procurement\TransferController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+/**
+ * Customers
+ */
 Route::get('/customers/register', [CustomerController::class, 'create']);
 Route::post('/customers/register', [CustomerController::class, 'store']);
 
+/**
+ * Coupons
+ */
 Route::get('/coupons/issue', [CouponController::class, 'create']);
 Route::post('/coupons/issue', [CouponController::class, 'store']);
 
+/**
+ * Loyalty Points
+ */
 Route::get('/loyalty/add', [LoyaltyPointController::class, 'create']);
 Route::post('/loyalty/add', [LoyaltyPointController::class, 'store']);
+
+/**
+ * Employees
+ */
 Route::get('/employees/create', [EmployeeController::class, 'create']);
 Route::post('/employees', [EmployeeController::class, 'store']);
 Route::get('/employees/{employee}/salary', [EmployeeController::class, 'salary']);
 
+/**
+ * Shift Schedules
+ */
 Route::get('/schedules/create', [ShiftScheduleController::class, 'create']);
 Route::post('/schedules', [ShiftScheduleController::class, 'store']);
 
+/**
+ * Attendance
+ */
 Route::get('/attendance', [AttendanceController::class, 'create']);
 Route::post('/attendance', [AttendanceController::class, 'store']);
 Route::post('/attendance/clock-out', [AttendanceController::class, 'clockOut']);
+
+/**
+ * Kitchen Display System
+ */
 Route::view('/kds', 'kds');
+
+/**
+ * Inventory
+ */
 Route::prefix('inventory')->group(function () {
     Route::resource('products', ProductController::class);
     Route::resource('recipes', RecipeController::class);
 });
+
+/**
+ * Procurement
+ */
 Route::prefix('procurement')->group(function () {
     Route::resource('suppliers', SupplierController::class);
     Route::resource('purchase-orders', PurchaseOrderController::class);
@@ -49,7 +86,11 @@ Route::prefix('procurement')->group(function () {
     Route::post('transfers', [TransferController::class, 'store']);
     Route::post('transfers/{stockTransfer}/approve', [TransferController::class, 'approve']);
     Route::post('transfers/{stockTransfer}/deliver', [TransferController::class, 'deliver']);
+});
 
+/**
+ * Two-Factor Authentication
+ */
 Route::middleware('auth')->prefix('two-factor')->group(function () {
     Route::post('enable', [TwoFactorController::class, 'enable']);
     Route::post('verify', [TwoFactorController::class, 'verify']);
